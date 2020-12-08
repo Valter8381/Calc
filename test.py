011ab6bfaf0228ca605b4919446fac656d467a94
discoroveryx <discoroveryx@gmail.com>
2020-12-08 16:19:52 +0700

    fix

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
-#Калькулятор ver 2
+"""
+Калькулятор ver 2
+"""
+
+# TODO изучить pep8
+# TODO изучить flake8
+# TODO почитать docstring
+# TODO почитать docstring
+# TODO заменить все ковычки на одинарные
+# TODO убрать всю табуляцию заменить на точки/пробелы 4
+# TODO строки складываються через format или f строки
+# TODO по pep8 в конце файла всегда перенос строки
+# TODO убрать все лишние символы в конце любой строки
+# TODO по возможности переделать работу на фукнцию
 
 from colorama import init
-from colorama import Fore, Back, Style
+from colorama import Fore
+from colorama import Back
+# from colorama import Style
+
 
 # use Colorama to make Termcolor work on Windows too
 init()
 
-print( Fore.BLACK )
-print( Back.GREEN )
+print(Fore.BLACK)  # noqa
+print(Back.GREEN)
 
 what = input("Что делаем? (+,-):")
 
@@ -16,15 +32,17 @@ print( Back.CYAN )
 a = float(input("Введи первое число:") )
 b = float(input("Введи второе число:") )
 
-print( Back.YELLOW )
+print( Back.YELLOW )  # noqa
 
 if what == "+":
-	c = a + b
-	print("Результат: " + str(c))
+    c = a + b
+    print("Результат: {}".format(
+        str(c)
+    )
 elif what == "-":
 	c = a - b
 	print("Результат: " + str(c))		 
 else:
-	print("Выбрана неверная операция!")	
+    print("Выбрана неверная операция!")	
 
 input()
